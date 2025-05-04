be92002a41e40bf4f284295679c51fa999e9b3da
stpetrakov <stpetrakov@yandex.ru>
2025-05-04 17:54:52 +0300

    Rewrite Contest1TaskA.cpp with O(p) memory

--- Contest1TaskA.cpp
+++ Contest1TaskA.cpp
@@ -1,44 +1,45 @@
-#include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 
-std::vector<size_t> prefix_function (std::string str) 
-{
-    size_t len =  str.length();
-    std::vector<size_t> pi(len);
-
-    for (size_t i = 1; i < len; ++i) 
-    {
-        size_t j = pi[i-1];
-        while ((j > 0) && (str[i] != str[j]))
+std::vector<size_t> prefix_function(const std::string &s) {
+    size_t n = s.size();
+    std::vector<size_t> pi(n);
+    for (size_t i = 1; i < n; ++i) {
+        size_t j = pi[i - 1];
+        while (j > 0 && s[i] != s[j]) {
             j = pi[j - 1];
-
-        if (str[i] == str[j]) 
+        }
+        if (s[i] == s[j]) {
             ++j;
-
+        }
         pi[i] = j;
-     }
-     return pi;
-} 
+    }
+    return pi;
+}
+
+void solve(const std::string &pattern, const std::string &text) {
+    size_t m = pattern.size();
+    auto pi = prefix_function(pattern);
+    size_t j = 0;
+    for (size_t i = 0; i < text.size(); ++i) {
+        while (j > 0 && text[i] != pattern[j]) {
+            j = pi[j - 1];
+        }
+        if (text[i] == pattern[j]) {
+            ++j;
+        }
+        if (j == m) {
+            std::cout << (i - m + 1) << "\n";
+            j = pi[j - 1];
+        }
+    }
+}
 
 int main() {
-    std::string str_a;
-    std::string str_b;
+    std::string str_a, str_b;
+    std::cin >> str_a >> str_b;
 
-    std::cin >> str_a;
-    std::cin >> str_b;
-
-    std::string str = str_b + "@" + str_a;
-    std::vector<size_t> pref;
-
-    pref = prefix_function(str);
-    size_t len_b = str_b.length();
-
-    for (size_t i = 0; i < pref.size(); ++i) {
-        if (pref[i] == len_b)
-            std::cout << i - len_b * 2 << '\n';
-    }
-
+    solve(str_b, str_a);
     return 0;
 }
