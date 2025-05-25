381f7330e0f8c40762fe2620ecb343db519a4348
stpetrakov <stpetrakov@yandex.ru>
2025-05-25 17:29:46 +0300

    Contest3TaskF

--- Contest3TaskF.cpp
+++ Contest3TaskF.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 #include <cstdint>
 #include <iomanip>
@@ -6,184 +7,236 @@
 #include <unordered_map>
 #include <vector>
 
+namespace math3d {
 
-struct Vec3 {
-  double x, y, z;
-  Vec3 operator+(const Vec3 &o) const { return {x + o.x, y + o.y, z + o.z}; }
-  Vec3 operator-(const Vec3 &o) const { return {x - o.x, y - o.y, z - o.z}; }
-  Vec3 operator/(double k) const { return {x / k, y / k, z / k}; }
+constexpr double kEpsilon = 1e-9;
+
+struct Vector3 {
+  double x{};
+  double y{};
+  double z{};
+
+  [[nodiscard]] Vector3 operator+(const Vector3 &other) const noexcept {
+    return {x + other.x, y + other.y, z + other.z};
+  }
+  [[nodiscard]] Vector3 operator-(const Vector3 &other) const noexcept {
+    return {x - other.x, y - other.y, z - other.z};
+  }
+  [[nodiscard]] Vector3 operator/(double k) const noexcept {
+    return {x / k, y / k, z / k};
+  }
 };
 
-double dot(const Vec3 &a, const Vec3 &b) {
+[[nodiscard]] inline double Dot(const Vector3 &a, const Vector3 &b) noexcept {
   return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
-Vec3 cross(const Vec3 &a, const Vec3 &b) {
+[[nodiscard]] inline Vector3 Cross(const Vector3 &a, const Vector3 &b) noexcept {
   return {a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x};
 }
 
+}
+
+struct InputData {
+  std::vector<math3d::Vector3> points;
+  std::vector<math3d::Vector3> queries;
+};
+
+InputData ReadInput();
+void WriteOutput(const std::vector<double> &answers);
+
+namespace hull {
+
+using math3d::Cross;
+using math3d::Dot;
+using math3d::Vector3;
+using math3d::kEpsilon;
+using VertexIndex = int;
+
 struct Face {
-  int a, b, c;
-  Vec3 n;
-  double d;
-  bool dead = false;
+  VertexIndex vertexA{0};
+  VertexIndex vertexB{0};
+  VertexIndex vertexC{0};
+  Vector3 normal;
+  double offset{0.0};
+  bool isDeleted{false};
 };
 
 struct Plane {
-  Vec3 n;
-  double len;
-  double d;
+  Vector3 normal;
+  double normalLength{1.0};
+  double offset{0.0};
 };
 
-constexpr double EPS = 1e-9;
-
-struct InputData {
-  std::vector<Vec3> points;
-  std::vector<Vec3> queries;
-};
-
-InputData read_input() {
-  int n;
-  std::cin >> n;
-  std::vector<Vec3> pts(n);
-  for (auto &p : pts)
-    std::cin >> p.x >> p.y >> p.z;
-
-  int q;
-  std::cin >> q;
-  std::vector<Vec3> queries(q);
-  for (auto &v : queries)
-    std::cin >> v.x >> v.y >> v.z;
-
-  return {std::move(pts), std::move(queries)};
-}
-
-void write_output(const std::vector<double> &ans) {
-  std::cout.setf(std::ios::fixed);
-  std::cout << std::setprecision(9);
-  for (double v : ans)
-    std::cout << v << '\n';
-}
-
-inline uint64_t edge_key(int u, int v) {
+constexpr uint64_t MakeEdgeKey(VertexIndex u, VertexIndex v) noexcept {
   return (static_cast<uint64_t>(std::min(u, v)) << 32) |
          static_cast<uint32_t>(std::max(u, v));
 }
 
-void add_face(std::vector<Face> &faces, int a, int b, int c,
-              const std::vector<Vec3> &pts, const Vec3 &inner_point) {
-  Vec3 n = cross(pts[b] - pts[a], pts[c] - pts[a]);
-  double d = -dot(n, pts[a]);
+std::array<VertexIndex, 4> FindInitialTetrahedron(const std::vector<Vector3> &pts) {
+  const int n = static_cast<int>(pts.size());
+  VertexIndex v0 = 0, v1 = 0, v2 = 0, v3 = 0;
 
-  if (dot(n, inner_point) + d > 0) {
-    std::swap(b, c);
-    n = {-n.x, -n.y, -n.z};
-    d = -d;
-  }
-  faces.push_back({a, b, c, n, d});
-}
-
-void find_initial_tetra(const std::vector<Vec3> &p, int &i0, int &i1, int &i2,
-                        int &i3) {
-  const int n = static_cast<int>(p.size());
-  i0 = 0;
-  for (i1 = 1; i1 < n; ++i1)
-    if (p[i1].x != p[i0].x || p[i1].y != p[i0].y || p[i1].z != p[i0].z)
+  for (v1 = 1; v1 < n; ++v1)
+    if (pts[v1].x != pts[v0].x || pts[v1].y != pts[v0].y || pts[v1].z != pts[v0].z)
       break;
 
-  for (i2 = i1 + 1; i2 < n; ++i2) {
-    Vec3 cp = cross(p[i1] - p[i0], p[i2] - p[i0]);
-    if (std::fabs(cp.x) + std::fabs(cp.y) + std::fabs(cp.z) > EPS)
+  for (v2 = v1 + 1; v2 < n; ++v2) {
+    const auto cp = Cross(pts[v1] - pts[v0], pts[v2] - pts[v0]);
+    if (std::fabs(cp.x) + std::fabs(cp.y) + std::fabs(cp.z) > kEpsilon)
       break;
   }
 
-  Vec3 base_n = cross(p[i1] - p[i0], p[i2] - p[i0]);
-  for (i3 = i2 + 1; i3 < n; ++i3)
-    if (std::fabs(dot(p[i3] - p[i0], base_n)) > EPS)
+  const auto baseNormal = Cross(pts[v1] - pts[v0], pts[v2] - pts[v0]);
+  for (v3 = v2 + 1; v3 < n; ++v3)
+    if (std::fabs(Dot(pts[v3] - pts[v0], baseNormal)) > kEpsilon)
       break;
+
+  return {v0, v1, v2, v3};
 }
 
-std::vector<Plane> build_convex_hull(const std::vector<Vec3> &pts) {
-  int n = static_cast<int>(pts.size());
-  int i0, i1, i2, i3;
-  find_initial_tetra(pts, i0, i1, i2, i3);
+void AddFace(std::vector<Face> &faces, VertexIndex va, VertexIndex vb, VertexIndex vc,
+             const std::vector<Vector3> &pts, const Vector3 &innerPoint) {
+  Vector3 normal = Cross(pts[vb] - pts[va], pts[vc] - pts[va]);
+  double offset = -Dot(normal, pts[va]);
+
+  if (Dot(normal, innerPoint) + offset > 0) {
+    std::swap(vb, vc);
+    normal = {-normal.x, -normal.y, -normal.z};
+    offset = -offset;
+  }
+  faces.push_back({va, vb, vc, normal, offset, false});
+}
+
+std::vector<VertexIndex> CollectVisibleFaces(const std::vector<Face> &faces,
+                                             const std::vector<Vector3> &pts,
+                                             VertexIndex pointId) {
+  std::vector<VertexIndex> visible;
+  for (VertexIndex faceId = 0; faceId < static_cast<VertexIndex>(faces.size()); ++faceId) {
+    const Face &face = faces[faceId];
+    if (!face.isDeleted && Dot(face.normal, pts[pointId]) + face.offset > kEpsilon)
+      visible.push_back(faceId);
+  }
+  return visible;
+}
+
+std::unordered_map<uint64_t, int> MarkFacesDeletedAndCollectBorder(
+    std::vector<Face> &faces, const std::vector<VertexIndex> &visibleFaces) {
+  std::unordered_map<uint64_t, int> borderUsage;
+  for (VertexIndex faceId : visibleFaces) {
+    Face &face = faces[faceId];
+    face.isDeleted = true;
+    const std::array<VertexIndex, 3> vertices = {face.vertexA, face.vertexB, face.vertexC};
+    for (int edgeIndex = 0; edgeIndex < 3; ++edgeIndex) {
+      uint64_t key = MakeEdgeKey(vertices[edgeIndex], vertices[(edgeIndex + 1) % 3]);
+      ++borderUsage[key];
+    }
+  }
+  return borderUsage;
+}
+
+void AddHorizonFaces(std::vector<Face> &faces,
+                     const std::unordered_map<uint64_t, int> &borderUsage,
+                     VertexIndex newPoint,
+                     const std::vector<Vector3> &pts,
+                     const Vector3 &innerPoint) {
+  for (const auto &[key, count] : borderUsage) {
+    if (count != 1)
+      continue;
+    VertexIndex u = static_cast<VertexIndex>(key >> 32);
+    VertexIndex v = static_cast<VertexIndex>(key & 0xffffffffu);
+    AddFace(faces, u, v, newPoint, pts, innerPoint);
+  }
+}
+
+std::vector<Plane> BuildConvexHull(const std::vector<Vector3> &pts) {
+  const auto tetra = FindInitialTetrahedron(pts);
+  const VertexIndex v0 = tetra[0], v1 = tetra[1], v2 = tetra[2], v3 = tetra[3];
 
   std::vector<Face> faces;
-  Vec3 inner = (pts[i0] + pts[i1] + pts[i2] + pts[i3]) / 4.0;
+  const Vector3 innerPoint = (pts[v0] + pts[v1] + pts[v2] + pts[v3]) / 4.0;
 
-  add_face(faces, i0, i1, i2, pts, inner);
-  add_face(faces, i0, i3, i1, pts, inner);
-  add_face(faces, i0, i2, i3, pts, inner);
-  add_face(faces, i1, i3, i2, pts, inner);
+  AddFace(faces, v0, v1, v2, pts, innerPoint);
+  AddFace(faces, v0, v3, v1, pts, innerPoint);
+  AddFace(faces, v0, v2, v3, pts, innerPoint);
+  AddFace(faces, v1, v3, v2, pts, innerPoint);
 
-  for (int idx = 0; idx < n; ++idx) {
-    if (idx == i0 || idx == i1 || idx == i2 || idx == i3)
+  const int n = static_cast<int>(pts.size());
+  for (VertexIndex p = 0; p < n; ++p) {
+    if (p == v0 || p == v1 || p == v2 || p == v3)
       continue;
 
-    std::vector<int> visible;
-    for (int f = 0; f < static_cast<int>(faces.size()); ++f)
-      if (!faces[f].dead && dot(faces[f].n, pts[idx]) + faces[f].d > EPS)
-        visible.push_back(f);
-
-    if (visible.empty())
+    auto visibleFaces = CollectVisibleFaces(faces, pts, p);
+    if (visibleFaces.empty())
       continue;
 
-    std::unordered_map<uint64_t, int> edgeCnt;
-    for (int fid : visible) {
-      Face &fc = faces[fid];
-      fc.dead = true;
-
-      int v[3] = {fc.a, fc.b, fc.c};
-      for (int e = 0; e < 3; ++e) {
-        uint64_t key = edge_key(v[e], v[(e + 1) % 3]);
-        ++edgeCnt[key];
-      }
-    }
-
-    for (const auto &kv : edgeCnt)
-      if (kv.second == 1) {
-        int u = static_cast<int>(kv.first >> 32);
-        int v = static_cast<int>(kv.first & 0xffffffffu);
-        add_face(faces, u, v, idx, pts, inner);
-      }
+    auto borderUsage = MarkFacesDeletedAndCollectBorder(faces, visibleFaces);
+    AddHorizonFaces(faces, borderUsage, p, pts, innerPoint);
   }
 
-  std::vector<Plane> pl;
-  for (const auto &f : faces)
-    if (!f.dead) {
-      double len = std::sqrt(dot(f.n, f.n));
-      pl.push_back({f.n, len, f.d});
-    }
-  return pl;
-}
-
-double distance_to_hull_point(const std::vector<Plane> &pl, const Vec3 &q) {
-  double best = std::numeric_limits<double>::max();
-  for (const auto &pln : pl) {
-    double signed_dist = dot(pln.n, q) + pln.d;
-    double dist = -signed_dist / pln.len;
-    if (dist < best)
-      best = dist;
+  std::vector<Plane> planes;
+  planes.reserve(faces.size());
+  for (const auto &face : faces) {
+    if (face.isDeleted)
+      continue;
+    double length = std::sqrt(Dot(face.normal, face.normal));
+    planes.push_back({face.normal, length, face.offset});
   }
-  return best;
+  return planes;
 }
 
-std::vector<double> answer_queries(const std::vector<Plane> &pl,
-                                   const std::vector<Vec3> &queries) {
-  std::vector<double> ans;
-  ans.reserve(queries.size());
+double DistanceToHull(const std::vector<Plane> &planes, const Vector3 &query) {
+  double minDistance = std::numeric_limits<double>::max();
+  for (const auto &plane : planes) {
+    double signedDist = Dot(plane.normal, query) + plane.offset;
+    double dist = -signedDist / plane.normalLength;
+    minDistance = std::min(minDistance, dist);
+  }
+  return minDistance;
+}
+
+std::vector<double> AnswerQueries(const std::vector<Plane> &planes,
+                                  const std::vector<Vector3> &queries) {
+  std::vector<double> result;
+  result.reserve(queries.size());
   for (const auto &q : queries)
-    ans.push_back(distance_to_hull_point(pl, q));
-  return ans;
+    result.push_back(DistanceToHull(planes, q));
+  return result;
+}
+
+}
+
+InputData ReadInput() {
+  using math3d::Vector3;
+  std::size_t pointCount;
+  std::cin >> pointCount;
+  std::vector<Vector3> points(pointCount);
+  for (auto &p : points)
+    std::cin >> p.x >> p.y >> p.z;
+
+  std::size_t queryCount;
+  std::cin >> queryCount;
+  std::vector<Vector3> queries(queryCount);
+  for (auto &q : queries)
+    std::cin >> q.x >> q.y >> q.z;
+
+  return {std::move(points), std::move(queries)};
+}
+
+void WriteOutput(const std::vector<double> &answers) {
+  std::cout.setf(std::ios::fixed);
+  std::cout << std::setprecision(9);
+  for (double v : answers)
+    std::cout << v << '\n';
 }
 
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  InputData data = read_input();
-  std::vector<Plane> hull_planes = build_convex_hull(data.points);
-  std::vector<double> ans = answer_queries(hull_planes, data.queries);
-  write_output(ans);
+  const InputData data = ReadInput();
+  const auto planes = hull::BuildConvexHull(data.points);
+  const auto distances = hull::AnswerQueries(planes, data.queries);
+  WriteOutput(distances);
   return 0;
 }
